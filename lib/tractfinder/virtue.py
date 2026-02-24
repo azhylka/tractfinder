210f068d82cc7a4804d7d93db5defc4d8f57f999
Andrey Zhylka <zhylka.ai@gmail.com>
2026-02-24 14:08:08 +0100

    Extracting deformation field computation
    Need more or less pure function to pass matrices as input.

--- lib/tractfinder/virtue.py
+++ lib/tractfinder/virtue.py
@@ -17,62 +17,66 @@ import gc
 # Compute and save deformation from file paths
 # Everything handled in mrtrix3 mif format
 def entry_point(tumour_mask, brain_mask, out_path, **kwargs):
-  tumour = load_mrtrix(tumour_mask)
-  brain  = load_mrtrix(brain_mask)
+    tumour = load_mrtrix(tumour_mask)
+    brain  = load_mrtrix(brain_mask)
 
-  imshape = brain.data.shape
-  assert tumour.data.shape == imshape, "Dimension mismatch"
+    imshape = brain.data.shape
+    assert tumour.data.shape == imshape, "Dimension mismatch"
 
-  tumour = np.logical_and(tumour.data, brain.data)
+    tumour = np.logical_and(tumour.data, brain.data)
 
-  save_lookup = kwargs['save_lookup']
-  # Load Db/Dt lookup arrays if available
-  Dt, Db = None, None
-  if save_lookup:
-    DbDt_path = os.path.join(save_lookup, 'DbDt.npz')
-    if os.path.exists(DbDt_path):
-      lookups_zipped = np.load(DbDt_path)
-      Db, Dt = lookups_zipped['Db'], lookups_zipped['Dt']
-      try:
-        s = tuple(lookups_zipped['shape'])
-        if not (s == imshape and np.prod(imshape) == Db.size == Dt.size):
-          Dt, Db = None, None
-      except KeyError:
-        if not np.prod(imshape) == Db.size == Dt.size:
-          Dt, Db = None, None
-      finally:
-        if Dt is None or Db is None:
-          app.warn("Precomputed Db and Dt don't match input brain mask grid. They will be recomputed and overwritten")
-    else:
-      # TODO: Deprecate individual file support
-      Dt_path = os.path.join(save_lookup, 'Dt.npy')
-      Db_path = os.path.join(save_lookup, 'Db.npy')
-      if os.path.exists(Dt_path): Dt = np.load(Dt_path)
-      if os.path.exists(Db_path): Db = np.load(Db_path)
-      try:
-        if not np.prod(imshape) == Db.size == Dt.size:
-          Dt, Db = None, None
-          app.warn("Precomputed Db and Dt don't match input brain mask grid. They will be recomputed and overwritten")
-      except AttributeError:
-        Dt, Db = None, None
+    save_lookup = kwargs['save_lookup']
+    # Load Db/Dt lookup arrays if available
+    D = create_deformation_field_vox(tumour, brain.data, imshape, save_lookup, **kwargs)
 
-  # All defaults
-  D = compute_radial_deformation(imshape, tumour, brain.data, Db=Db, Dt=Dt, **kwargs)
+    l = kwargs.get('expon')
+    k = 'exponential ' if l else 'linear'
+    if l == -1: k += '(adaptive)'
+    elif kwargs.get('expon_const'): k += f'(lambda = {l})'
+    elif l: k += f'(adaptive, lambda_min = {l})'
+    out = Image.empty_as(brain)
+    out.comments = [f"scale factor: {kwargs.get('squish', 1)}", f"deformation factor: {k}"]
+    # Black magic that apparently I wrote
+    # Convert deformation values from voxel coords to scanner coordinates (mm)
+    # and reshape to voxel array
+    out.data = (np.hstack((out.vox * D, np.ones((max(D.shape), 1))))
+                @ out.transform.T)[:,:3].reshape(*imshape, 3)
 
-  l = kwargs.get('expon')
-  k = 'exponential ' if l else 'linear'
-  if l == -1: k += '(adaptive)'
-  elif kwargs.get('expon_const'): k += f'(lambda = {l})'
-  elif l: k += f'(adaptive, lambda_min = {l})'
-  out = Image.empty_as(brain)
-  out.comments = [f"scale factor: {kwargs.get('squish', 1)}", f"deformation factor: {k}"]
-  # Black magic that apparently I wrote
-  # Convert deformation values from voxel coords to scanner coordinates (mm)
-  # and reshape to voxel array
-  out.data = (np.hstack((out.vox * D, np.ones((max(D.shape), 1))))
-              @ out.transform.T)[:,:3].reshape(*imshape, 3)
+    save_mrtrix(out_path, out)
 
-  save_mrtrix(out_path, out)
+
+def create_deformation_field_vox(tumour, brain_data, imshape, save_lookup, **kwargs):
+    Dt, Db = None, None
+    if save_lookup:
+        DbDt_path = os.path.join(save_lookup, 'DbDt.npz')
+        if os.path.exists(DbDt_path):
+            lookups_zipped = np.load(DbDt_path)
+            Db, Dt = lookups_zipped['Db'], lookups_zipped['Dt']
+            try:
+                s = tuple(lookups_zipped['shape'])
+                if not (s == imshape and np.prod(imshape) == Db.size == Dt.size):
+                    Dt, Db = None, None
+            except KeyError:
+                if not np.prod(imshape) == Db.size == Dt.size:
+                    Dt, Db = None, None
+            finally:
+                if Dt is None or Db is None:
+                   app.warn("Precomputed Db and Dt don't match input brain mask grid. They will be recomputed and overwritten")
+        else:
+            # TODO: Deprecate individual file support
+            Dt_path = os.path.join(save_lookup, 'Dt.npy')
+            Db_path = os.path.join(save_lookup, 'Db.npy')
+            if os.path.exists(Dt_path): Dt = np.load(Dt_path)
+            if os.path.exists(Db_path): Db = np.load(Db_path)
+            try:
+                if not np.prod(imshape) == Db.size == Dt.size:
+                    Dt, Db = None, None
+                    app.warn("Precomputed Db and Dt don't match input brain mask grid. They will be recomputed and overwritten")
+            except AttributeError:
+                Dt, Db = None, None
+
+    # All defaults
+    return compute_radial_deformation(imshape, tumour, brain_data, Db=Db, Dt=Dt, **kwargs)
 
 
 # Get triangulated surface object from binary volume
